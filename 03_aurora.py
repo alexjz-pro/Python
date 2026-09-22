3ad624a069fb13a088faa11d2b00fea4dcfb2fd9
Kronnos <alexjzoficial1@gmail.com>
2026-09-22 11:23:41 -0400

    modelos actualizados por funciones

--- 03_aurora.py
+++ 03_aurora.py
@@ -9,10 +9,47 @@ Si aun no sabemos caminar... Atte: Axdev"""
 # Variables principales
 val_1 = 0 # Primer valor
 val_2 = 0 # Segundo valor
-resultado = 0 # resultado
 
 # Valores especiales
 ruta = False # Usaremos un boolean en false ya veran mas adelante para que
+
+# funciones suma, resta, multiplicacion, division, mayor menor o igual que
+def suma():
+    val_1 = int(input("introduzca el primer valor: \n"))
+    val_2 = int(input("introduzca el segundo valor: \n"))
+    print("-"*50)
+    print(val_1 + val_2)
+
+def resta():
+    val_1 = int(input("introduzca el primer valor: \n"))
+    val_2 = int(input("introduzca el segundo valor: \n"))
+    print("-"*50)
+    print(val_1 - val_2)
+
+def multiplicacion():
+    val_1 = int(input("introduzca el primer valor: \n"))
+    val_2 = int(input("introduzca el segundo valor: \n"))
+    print("-"*50)
+    print(val_1 * val_2)
+
+def division():
+    val_1 = int(input("introduzca el primer valor: \n"))
+    val_2 = int(input("introduzca el segundo valor: \n"))
+    print("-"*50)
+    print(f"{val_1 / val_2:.2f}")
+
+def mmq():
+    val_1 = float(input("introduzca el primer valor: \n"))
+    val_2 = float(input("introduzca el segundo valor: \n"))
+    if val_1 > val_2:
+        print("-"*50)
+        print(f"{val_1} es mayor que {val_2}")
+    elif val_2 > val_1:
+        print("-"*50)
+        print(f"{val_2} es mayor que {val_1}")
+    elif val_1 == val_2:
+            print("-"*50)
+            print(f"Los valores son iguales")
 
 print("-"*50)
 print("-"*18, "A U R O R A", "-"*19)
@@ -26,53 +63,27 @@ while ruta == False:
 
     if respuesta == "S":
         print("Sumar")
-        val_1 = int(input("ingrese el primer valor: \n"))
-        val_2 = int(input("ingrese el segundo valor: \n"))
-        resultado = val_1 + val_2
         print("-"*50)
-        print(f"{resultado}")
-        resultado = 0
+        suma()
 
     elif respuesta == "R":
         print("Restar")
-        val_1 = int(input("ingrese el primer valor: \n"))
-        val_2 = int(input("ingrese el segundo valor: \n"))
-        resultado = val_1 - val_2
         print("-"*50)
-        print(f"{resultado}")
-        resultado = 0
+        resta()
 
     elif respuesta == "M":
         print("Multiplicar")
-        val_1 = int(input("ingrese el primer valor: \n"))
-        val_2 = int(input("ingrese el segundo valor: \n"))
-        resultado = val_1 * val_2
         print("-"*50)
-        print(f"{resultado}")
-        resultado = 0
+        multiplicacion()
 
     elif respuesta == "D":
         print("Dividir")
-        val_1 = int(input("ingrese el primer valor: \n"))
-        val_2 = int(input("ingrese el segundo valor: \n"))
-        resultado = val_1 / val_2
         print("-"*50)
-        print(f"{resultado:.2f}")
-        resultado = 0
+        division()
 
     elif respuesta == "P":
         print("Mayor que")
-        val_1 = float(input("ingrese el primer valor: \n"))
-        val_2 = float(input("ingrese el segundo valor: \n"))
-        if val_1 > val_2:
-            print("-"*50)
-            print(f"{val_1} es mayor que {val_2}")
-        elif val_2 > val_1:
-            print("-"*50)
-            print(f"{val_2} es mayor que {val_1}")
-        elif val_1 == val_2:
-            print("-"*50)
-            print(f"Los valores son iguales")
+        mmq()
 
     elif respuesta == "N":
         ruta = True
